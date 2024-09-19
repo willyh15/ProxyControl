3beaf2e9e121782161a7671d5d0aec17ab44aced
Will Harrison <willharrison2016@gmail.com>
2024-09-19 12:16:22 -0500

    Update Program.cs

---
+++ src/AvaloniaApp/Program.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using System;
+
+namespace ProxyControlApp
+{
+    class Program
+    {
+        // Entry point for Avalonia application
+        public static void Main(string[] args)
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace();
+    }
+}
